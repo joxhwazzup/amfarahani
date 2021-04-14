71ba2be78bd7f4f10db57c95c6e9c6ce7b758064
Joshua Damian <82067104+joxhwazzup@users.noreply.github.com>
2021-04-13 20:19:08 -0400

    Update circle.cpp
    Edited version of circle.cpp

--- circle.cpp
+++ circle.cpp
@@ -6,10 +6,19 @@ using namespace std;
 
 int main()
 {
-	double radius;
-	cout << "Enter radius\n";
-	//Add code to read user input and store in variable radius
-	cout << "The area of a circle with radius \n";
-	//Calculate the area of the circle
-	return 0;
+    double radius;
+    double area;
+    
+    //Prompt the user to enter a value for radius
+    cout << "Enter radius ";
+    cin >> radius;
+    
+    //Calculate the area of the circle
+    area = 3.14 * radius * radius;
+    
+    //Add code to read user input and store in variable radius
+    cout << "The area of a circle is: " << area << endl;
+    
+    return 0;
+    
 }
