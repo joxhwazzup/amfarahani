7f52efc57cc58ede19c34a5b4dc039de674bc6a7
joxhwazzup <j.damian8703@student.nu.edu>
2021-05-01 23:46:57 -0400

    update files

---
+++ circle_circumference.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double radius;
+    double circumference;
+    
+    //Prompt the user to enter a value for radius
+    cout << "Enter radius ";
+    cin >> radius;
+    
+    //Calculate the area of the circle
+    circumference = 2.0 * 3.14 * radius;
+    
+    //Add code to read user input and store in variable radius
+    cout << "The circumference of a circle is: " << circumference << endl;
+    
+    return 0;
+    
+}
+
